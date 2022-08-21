a3a700f678ffd6d6b7c28e829393781dcab1b4bd
akshitgupta1204 <32005721+akshitgupta1204@users.noreply.github.com>
2022-08-21 10:04:15 -0700

    Update data.py

--- stats/data.py
+++ stats/data.py
@@ -1,23 +1,53 @@
-from cmath import nan
-import os, glob
-import pandas as pd 
+# import os, glob
+# import pandas as pd 
 
-games_files = glob.glob(os.path.join(os.getcwd(), 'games', '*.EVE'))
-games_files.sort()
+# game_files = glob.glob(os.path.join(os.getcwd(), 'games', '*.EVE'))
+# game_files.sort()
+
+# game_frames = []
+# for game_file in game_files:
+#     game_frame = pd.read_csv(game_file, names=['type', 'multi2', 'multi3', 'multi4', 'multi5', 'multi6', 'event'])
+#     game_frames.append(game_frame)
+# games = pd.concat(game_frames)
+# games.loc[games['multi5'] == '??', ['multi5']] = ''
+
+# identifiers = games['multi2'].str.extract(r'(.LS(\d{4})\d{5})')
+
+# identifiers = identifiers.fillna(method='ffill')
+# identifiers.columns = ['game_id', 'year']
+
+# games = pd.concat([games, identifiers], axis=1, sort=False)
+# games = pd.fillna(' ')
+# games.loc[:, 'type'] = pd.Categorical(games.loc[:, 'type'])
+# print(games.head())
+
+
+
+import os
+import glob
+
+import pandas as pd
+
+
+game_files = glob.glob(os.path.join(os.getcwd(), "games", "*.EVE"))
+game_files.sort()
 
 game_frames = []
-for game_file in games_files:
+for game_file in game_files:
     game_frame = pd.read_csv(game_file, names=['type', 'multi2', 'multi3', 'multi4', 'multi5', 'multi6', 'event'])
     game_frames.append(game_frame)
-    games = pd.concat(game_frames)
-    pd.dataframe.loc['row condition', ['multi5']] = ''
-    identifiers = pd.dataframe[games['multi2']].str.extract(r'(.LS(\d{4})\d{5})')
 
-    identifiers = identifiers.fillna(method='ffill')
-    identifiers['columns'] = ['game_id', 'year']
+games = pd.concat(game_frames)
 
-    games = pd.concat([games, identifiers], axis=1, sort=False)
-    games = pd.fillna(nan, ' ')
-    pd.Categorical(games.loc[:])
-    print(pd.head())
+games.loc[games['multi5'] == '??', ['multi5']] = ''
+
+identifiers = games['multi2'].str.extract(r'(.LS(\d{4})\d{5})')
+identifiers = identifiers.fillna(method='ffill')
+identifiers.columns = ['game_id', 'year']
+
+games = pd.concat([games, identifiers], sort=False, axis=1)
+games = games.fillna(' ')
+games.loc[:, 'type'] = pd.Categorical(games.loc[:, 'type'])
+
+print(games.head())
 
