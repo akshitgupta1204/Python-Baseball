e8c9ced8f3e53bdee59ebeb0b9b28c0adc47c33f
akshitgupta1204 <32005721+akshitgupta1204@users.noreply.github.com>
2022-08-22 09:19:09 -0700

    Update offense.py

--- stats/offense.py
+++ stats/offense.py
@@ -17,8 +17,7 @@ r'^HR(.*)': 'hr'
 hit_type = hits['event'].replace(replacements, regex=True)
 
 hits = hits.assign(hit_type=hit_type)
-hits = hits.groupby(['inning', 'hit_type']).size()
-hits = hits.reset_index(name='count')
+hits = hits.groupby(['inning', 'hit_type']).size().reset_index(name='count')
 
 hits['hit_type'] = pd.Categorical(hits['hit_type'], ['single', 'double', 'triple', 'hr'])
 
